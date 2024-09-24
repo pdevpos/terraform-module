c6729ad08c7575d7fc39ac003d71b71b2cd62700
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-24 13:18:18 +0530

    name mistake

--- module/app/data.tf
+++ module/app/data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "example" {
   most_recent = true
   owners = ["973714476881"]
   filter {
-    name   = "AMI name"
+    name   = "name"
     values = ["RHEL-9-DevOps-Practice"]
   }
 }
