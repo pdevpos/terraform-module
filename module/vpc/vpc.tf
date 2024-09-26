ff81c3c5c362082de947ffe58a2a1f5fdbb49fba
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-27 00:33:36 +0530

    * create elastic ip address * allocate elastic ip to nat gateway * connect natgateway to public subnet

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -75,7 +75,7 @@ resource "aws_eip" "elastic_ip" {
 
 }
 #create nat gateway
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.elastic_ip.id
   subnet_id     = aws_subnet.public_subnet[0].id
   tags = {
@@ -91,6 +91,16 @@ resource "aws_route" "public_route" {
   route_table_id            = aws_route_table.public_route_table.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
+}
+# create a private route
+resource "aws_route" "private_route" {
+  route_table_id            = aws_route_table.private_route_table.id
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+# create a db route
+resource "aws_route" "db_route" {
+  route_table_id            = aws_route_table.db_route_table.id
+  nat_gateway_id = aws_nat_gateway.nat.id
 }
 
 # associate route table and subnets
