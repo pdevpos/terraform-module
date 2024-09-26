bbd7f6647455296b904f3b22f2b8e34f11546ff9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-26 23:24:36 +0530

    associate public route table and subnets

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -24,7 +24,7 @@ cidr_block = var.subnet_cidr_block[count.index]
     Name = "${local.tag_name}-subnet-${count.index}"
   }
 }
-resource "aws_route_table" "route_table" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   tags = merge(
     var.common_tags,
@@ -33,7 +33,12 @@ resource "aws_route_table" "route_table" {
   })
 }
 resource "aws_route" "route" {
-  route_table_id            = aws_route_table.route_table.id
+  route_table_id            = aws_route_table.public_route_table.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
+}
+resource "aws_route_table_association" "route_subnet_association" {
+  count = length(var.subnet_cidr_block)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
 }
