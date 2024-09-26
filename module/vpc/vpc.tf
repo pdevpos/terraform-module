be78c21b55fe7a150c88641658cbf000c0bc83d1
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-26 19:26:36 +0530

    * create routes(to follow some rules) * add destination cidr * add gateway id

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -34,5 +34,6 @@ resource "aws_route_table" "route_table" {
 }
 resource "aws_route" "route" {
   route_table_id            = aws_route_table.route_table.id
-  destination_cidr_block    = aws_internet_gateway.igw.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 }
