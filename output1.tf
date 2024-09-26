e607e75ed704a08833117b424d99e462b2c61886
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-26 16:45:06 +0530

    comment unnecessary code

--- output1.tf
+++ output1.tf
@@ -1,6 +1,6 @@
-output "env" {
-  value = var.env
-}
-output "avz" {
-  value = module.vpc_Aws.avz_extr
-}
+# output "env" {
+#   value = var.env
+# }
+# output "avz" {
+#   value = module.vpc_Aws.avz_extr
+# }
