5314bfab61216d975b936afaf4e60db0eacea90a
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-26 18:17:17 +0530

    create only vpc

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "vpc_info" {
-  value = module.vpc_mod.vpc
-}
-output "igw_info" {
-  value = module.vpc_mod.igw
-}
-output "avz"{
-  value = module.vpc_mod.av_region
-}
+# output "vpc_info" {
+#   value = module.vpc_mod.vpc
+# }
+# output "igw_info" {
+#   value = module.vpc_mod.igw
+# }
+# output "avz"{
+#   value = module.vpc_mod.av_region
+# }
