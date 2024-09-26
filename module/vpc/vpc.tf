c9c71f26b42aa92a7a30f40633165750ef3e6775
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-26 16:36:48 +0530

    * create vpc * get vpc info

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -1,26 +1,27 @@
-resource "aws_vpc" "vpc" {
-  cidr_block       = var.cidrBlock
-  enable_dns_hostnames = var.dns_hostnames
-   tags = merge(
-    var.common_tags,
-    {
-  Name = local.tag_name
-  })
-
-  }
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${local.tag_name}-igw"
-  }
-}
-resource "aws_subnet" "subnet" {
-  count = length(var.subnet_cidr_block)
-  availability_zone = local.avz[count.index]
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.subnet_cidr_block[count.index]
-  tags = {
-    Name = "${local.tag_name}-subnet-${count.index}"
-  }
-}
-
+# resource "aws_vpc" "vpc" {
+#   cidr_block       = var.cidrBlock
+#   enable_dns_hostnames = var.dns_hostnames
+#    tags = merge(
+#     var.common_tags,
+#     {
+#   Name = local.tag_name
+#   })
+#
+#   }
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "${local.tag_name}-igw"
+#   }
+# }
+# resource "aws_subnet" "public_subnet" {
+#   count = length(var.subnet_cidr_block)
+#   availability_zone = local.avz[count.index]
+#   vpc_id     = aws_vpc.vpc.id
+#   map_public_ip_on_launch = true
+# cidr_block = var.subnet_cidr_block[count.index]
+#   tags = {
+#     Name = "${local.tag_name}-subnet-${count.index}"
+#   }
+# }
+#
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-module "vpc_Aws" {
-  source = "./module/vpc"
-  cidrBlock = var.cidr_block
-  env = var.env
-  projectName = var.project_name
-  dns_hostnames = var.enable_dns_hostnames
-  subnet_cidr_block = var.subnet_cidr_block
-}
+# module "vpc_Aws" {
+#   source = "./module/vpc"
+#   cidrBlock = var.cidr_block
+#   env = var.env
+#   projectName = var.project_name
+#   dns_hostnames = var.enable_dns_hostnames
+#   subnet_cidr_block = var.subnet_cidr_block
+# }
