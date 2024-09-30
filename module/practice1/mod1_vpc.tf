9a1c4688b25b7ecb66410f4dc73ee69524c6392b
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-10-01 05:15:54 +0530

    Edit public route and attach igw

--- module/practice1/mod1_vpc.tf
+++ module/practice1/mod1_vpc.tf
@@ -89,4 +89,10 @@ resource "aws_route_table" "db_route" {
     }
   )
 }
+# create public route and attach igw
+resource "aws_route" "r" {
+  route_table_id         = aws_route_table.public_route.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
 
