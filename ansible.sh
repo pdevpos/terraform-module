4f4bdfc10ec121ebe8234c428e3e74f028cc259a
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-10-02 21:48:37 +0530

    user input validations

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,11 @@
 env=$1
 action=$2
+if [ -z "" ]; then
+  echo "Input env is missing"
+fi
+if [ -z "" ]; then
+  echo "Input action is missing"
+fi
 rm -rf .terraform/terraform.tfstate
 terraform init -backend-config=${env}-state/main.tfvars
 terraform $action -var-file=${env}-state/state.tfvars
