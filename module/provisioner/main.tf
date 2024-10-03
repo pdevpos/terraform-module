9e13f0ffb736609dda52622207dec1e9ec585bda
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-10-03 19:11:16 +0530

    structure changed

--- module/provisioner/main.tf
+++ module/provisioner/main.tf
@@ -1,19 +1,46 @@
+# resource "aws_instance" "instance" {
+#   ami           = data.aws_ami.ami.image_id
+#   instance_type = var.instance_type
+#   instance_market_options {
+#         market_type = "spot"
+#         spot_options {
+#           spot_instance_type = "persistent"
+#           instance_interruption_behavior = "stop"
+#         }
+#   }
+#   provisioner "remote-exec" {
+#     connection {
+#       type     = var.protocol_type
+#       user     = var.ssh_user
+#       password = var.ssh_password
+#       host     = self.public_ip
+#     }
+#     inline = [
+#       "sudo dnf install nginx -y"
+#     ]
+#   }
+# }
+# if there is any fail in provisioner first terraform instance will destroy existing instances and recreate a new instance
+#  so here we have to decouple the provisioner with null resource
+# if provisioner fails terraform instance will not destroy and recreate
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   instance_market_options {
-        market_type = "spot"
-        spot_options {
-          spot_instance_type = "persistent"
-          instance_interruption_behavior = "stop"
-        }
+    market_type = "spot"
+    spot_options {
+      spot_instance_type             = "persistent"
+      instance_interruption_behavior = "stop"
+    }
   }
+}
+resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
     connection {
       type     = var.protocol_type
       user     = var.ssh_user
       password = var.ssh_password
-      host     = self.public_ip
+      host     = aws_instance.instance.public_ip
     }
     inline = [
       "sudo dnf install nginx -y"
